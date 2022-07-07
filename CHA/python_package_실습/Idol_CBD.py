4dd88ce37e48b0afec2610e807f471c157e26583
notebook_CBD <orientpine@gmail.com>
2022-07-07 16:20:22 -0400

    수업 끝

---
+++ CHA/python_package_실습/Idol_CBD.py
@@ -0,0 +1,16 @@
+class girlGroup:
+    def __init__(self, team: str, member: list):
+        self.team = team
+        self.member = member
+
+    def sayHello(self):
+        print(f"안녕하세요, 우리는 {self.team}입니다!")
+
+    def introduce(self):
+        print(f"우리 팀에는 {','.join(self.member)} 있습니다.")
+
+    def plot(self):
+        ~~~~~~~~~~~~~~~~~~~~`
+        ~~~~~~~~~~~~~~~~~~~
+        ~~~~~~~~~~`
+        `
